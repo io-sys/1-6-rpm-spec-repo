a8d8a4f757a8e7c683ed72f5c4bd69b92ab09389
io-sys <47218309+io-sys@users.noreply.github.com>
2019-04-28 07:04:38 +0300

    Update 01_curl_SSL.sh

--- 01_curl_SSL.sh
+++ 01_curl_SSL.sh
@@ -1,6 +1,5 @@
-# 01_curl_SSL.sh
-# rpm spec +ssl
-#
+#### 01_curl_SSL.sh
+#### tegs: rpm spec ssl repo 
 #
 # Понадобятся следующие установленные пакеты:
 yum install -y \
@@ -17,26 +16,22 @@ rpm-build \
 createrepo \
 yum-utils
 #
-#
 # Версия из repo - curl-7.29.0-51.el7.x86_64
 yum provides curl
-#
 #
 # В ~ директории создать каталог /rpmbuild с нужными подкаталогами, они будут пустые.
 rpmdev-setuptree
 cd ~/rpmbuild/SRPMS/
-# Загрузить SRPMS
+# Загрузить SRPMS программы curl
 yumdownloader --source curl-7.29.0
 # Достать spec и разархивировать вспомогательные файлы.
 rpm -ivh ~/rpmbuild/SRPMS/curl-7.29.0-51.el7.src.rpm
-#
 #
 # OpenSSL загружаю и распаковываю в директории /opt
 # Для загрузки OpenSSL отдельная папка
 mkdir /usr/src/OpenSSL && cd /usr/src/OpenSSL
 wget https://www.openssl.org/source/openssl-1.0.2r.tar.gz
 tar -xvf openssl-1.0.2r.tar.gz --directory=/opt/
-#
 #
 # Правится spec curl'а
 sed -i.bak01 's@--without-ssl@--with-ssl=/opt/openssl-1.0.2r@' ~/rpmbuild/SPECS/curl.spec
